e3275f3d99276a889c1f24f9a080ac6ea6e84632
Sergey <semabrest@gmail.com>
2022-12-03 22:40:10 +0300

    customs files

---
+++ js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+// // alert('Hello');
+// // const result = confirm("Are you here?");
+// // console.log(result);
+// const answer = prompt("Вам есть 18?", " ");
+// console.log(typeof(answer));
+// const answers = [];
+// answers [0] = prompt('Как ваше имя', '');
+// answers [1] = prompt('Как ваше фамилия?', '');
+// answers [2] = prompt('Сколько Вам лет?', '');
+// console.log(typeof(answers));
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5` );
+// const user = "Ivan";
+// alert(`Привет, ${user}`);
+console.log('arr' + ' - object');
+console.log(4 + ' - object');
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+console.log(++incr);
+console.log(--decr);
+console.log(2 + 2 *2 !== '6');
+const isChecked = true,
+        isClose = false;
+       console.log(isChecked || isClose); 
+
